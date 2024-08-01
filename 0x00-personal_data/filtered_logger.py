b75c428c6d0096804081606453bddc30b06ff4be
Code021-raelle <gabrielakinshola3@gmail.com>
2024-08-01 14:34:09 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,9 @@ Filtered Logger Module
 """
 import logging
 import re
+import os
+import mysql.connector
+from mysql.connector import connection
 from typing import List, Tuple
 
 
@@ -16,6 +19,21 @@ def filter_datum(
         '|'.join([f'{field}=[^{separator}]*' for field in fields]),
         lambda m: f"{m.group().split('=')[0]}={redaction}",
         message
+    )
+
+
+def get_db() -> connection.MySQLConnection:
+    """ Returns a connection to the database. """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
     )
 
 
