a2bb0ec379251f60fb64f950a4f9ed908baebf21
Code021-raelle <gabrielakinshola3@gmail.com>
2024-08-01 13:24:18 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,9 @@ from typing import List
 
 
 def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str) -> str:
+        fields: List[str],
+        redaction: str,
+        message: str, separator: str) -> str:
     """ Returns the log message obfuscated. """
     return re.sub(
         '|'.join([f'{field}=[^{separator}]*' for field in fields]),
