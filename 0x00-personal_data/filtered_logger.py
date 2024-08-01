773b6e422f16bd0e2c5e2ad25c865c568a007a4e
Code021-raelle <gabrielakinshola3@gmail.com>
2024-08-01 13:23:35 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ Returns the log message obfuscated. """
     return re.sub(
         '|'.join([f'{field}=[^{separator}]*' for field in fields]),
